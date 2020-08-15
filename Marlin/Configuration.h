e2e1bd79fb63456b8e4217529fab28aab5acd519
Iochhi <iochhi@hotmail.com>
2020-08-15 13:13:58 -0400

    Fix M851 a 0.80

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -993,7 +993,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 38, 19, -0.90 }
+#define NOZZLE_TO_PROBE_OFFSET { 38, 19, -0.80 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
