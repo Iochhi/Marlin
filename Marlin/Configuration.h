ab61cb9c9c154aaead880fb252549010cacb6a7d
Iochhi <iochhi@hotmail.com>
2020-10-20 16:29:21 -0300

    custon name

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -131,7 +131,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-//#define CUSTOM_MACHINE_NAME "FC C3 201020 ABL 2.0.7.2"
+#define CUSTOM_MACHINE_NAME "FC C3 201020 ABL 2.0.7.2"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
