6de107513f7176027100bec1d263f11d5b8852e0
Iochhi <iochhi@hotmail.com>
2020-10-28 19:29:26 -0300

    Ajustes Fans

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -131,7 +131,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "C2 2.0.7.2 151020"
+#define CUSTOM_MACHINE_NAME "C2 2.0.7.2 281020"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
