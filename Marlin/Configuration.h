f71e63610578aacc78af0536dc75d6c557c4ac42
Iochhi <iochhi@hotmail.com>
2023-06-26 19:42:36 -0400

    Fecha

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -115,7 +115,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "C1 2.1.2.1 2006-23"
+#define CUSTOM_MACHINE_NAME "C1 2.1.2.1 2606-23"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
