94595567ea3fe560f85caa5f853726df7dd6a0ed
Iochhi <iochhi@hotmail.com>
2023-07-10 00:00:24 -0400

    PFF

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1350,7 +1350,7 @@
  *    - Normally-closed (NC) also connect to GND.
  *    - Normally-open (NO) also connect to 5V.
  */
-#define Z_MIN_PROBE_PIN PC14 //-1
+//#define Z_MIN_PROBE_PIN -1
 
 /**
  * Probe Type
