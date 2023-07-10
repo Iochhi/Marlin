0cc83172f5cef5f09a0deb907e56d3d73b0b9c5e
Iochhi <iochhi@hotmail.com>
2023-07-10 11:39:40 -0400

    DIRECT_PIN_CONTROL

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -4438,12 +4438,12 @@
 //
 // M42 - Set pin states
 //
-//#define DIRECT_PIN_CONTROL
+#define DIRECT_PIN_CONTROL //Quitar despues de Probar pines
 
 //
 // M43 - display pin status, toggle pins, watch pins, watch endstops & toggle LED, test servo probe
 //
-//#define PINS_DEBUGGING
+#define PINS_DEBUGGING //Quitar despues de Probar pines
 
 // Enable Tests that will run at startup and produce a report
 //#define MARLIN_TEST_BUILD
