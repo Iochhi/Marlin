8fac28d7817a6e3355b8f692d1bd66441ca2c2f8
Iochhi <iochhi@hotmail.com>
2023-07-11 23:17:50 -0400

    DEFAULT_MAX_FEEDRATE Z a 12

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1233,7 +1233,7 @@
  * Override with M203
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 300, 300, 15, 200 }
+#define DEFAULT_MAX_FEEDRATE          { 300, 300, 12, 200 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
